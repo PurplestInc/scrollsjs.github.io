e796fe52609e32bcfbb902ace42e8b82b913ec5c
Rob Dukarski <robdukarski@users.noreply.github.com>
2018-07-26 13:15:55 -0400

    Add files
    Add all files.

---
+++ src/js/main.js
@@ -0,0 +1,87 @@
+/**
+ * @fileoverview main.js is used for adding base functionaltiy to the
+ *               scrolls.js website.
+ * @author Rob Dukarski <rob@purplest.com> (https://github.com/RobDukarski)
+ * @copyright 2018 Purplest, Inc.
+ * @version 1.0.0
+ */
+
+const bttButton = document.querySelector('.js-btt');
+const links = document.querySelectorAll('.js-nav a');
+
+/**
+ * Scrolls to the top of the page.
+ */
+
+const backToTop = () => {
+	scrolls(0);
+};
+
+/**
+ * Hides the back-to-top button.
+ */
+
+const hideBackToTop = () => {
+  if (window.pageYOffset < 20 && bttButton) {
+		bttButton.classList.remove('is--shown');
+	}
+};
+
+/**
+ * Shows the back-to-top button.
+ */
+
+const showBackToTop = () => {
+  if (window.pageYOffset >= 20 && bttButton) {
+		bttButton.classList.add('is--shown');
+
+		bttButton.addEventListener('click', () => {
+			backToTop();
+		});
+	}
+};
+
+/**
+ * Listens for the scroll event to occur and updates the back-to-top button
+ * accordingly.
+ */
+
+window.addEventListener('scroll', () => {
+	if (window.pageYOffset < 20) {
+		hideBackToTop();
+	} else {
+		showBackToTop();
+	}
+});
+
+/**
+ * Code setup to execute immediately including initializing events, and their
+ * liseners.
+ */
+
+(() => {
+	if (bttButton) {
+		bttButton.addEventListener('click', () => {
+			backToTop();
+		});
+
+		if (window.pageYOffset >= 20) {
+			bttButton.classList.add('is--shown');
+		}
+	}
+
+	if (links) {
+		let linksCount = links.length;
+
+		for (let i = 0; i < linksCount; i++) {
+			let tempLink = links[i];
+			let tempSection = tempLink.getAttribute('href').replace('#', '');
+
+			tempLink.addEventListener('click', (e) => {
+				e.preventDefault();
+
+				scrolls('.' + tempSection);
+			});
+		}
+	}
+})();

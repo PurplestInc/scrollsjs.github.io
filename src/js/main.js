0521b2a95d43c9007865b335d826080951b28c74
Rob Dukarski <robdukarski@users.noreply.github.com>
2018-07-30 13:59:27 -0400

    Fix main.js
    Fixed a typo.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 /**
- * @fileoverview main.js is used for adding base functionaltiy to the
+ * @fileoverview main.js is used for adding base functionality to the
  *               scrollsjs.com website.
  * @author Rob Dukarski <rob@purplest.com> (https://github.com/RobDukarski)
  * @copyright 2018 Purplest, Inc.
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 const bttButton = document.querySelector('.js-btt');
